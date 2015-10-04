e7f9cf9b1ea5ed378e44adffb65cb4f606264737
Yuval Hermelin <yuval.hermelin@gmail.com>
2015-10-03 19:24:54 -0700

    Change design more. (Spacing, alignment of text, add text to spinner)

--- app/src/main/java/com/yuvalhermelin/buildseasonmanager/MainActivity.java
+++ app/src/main/java/com/yuvalhermelin/buildseasonmanager/MainActivity.java
@@ -8,10 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
@@ -29,6 +31,9 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         mLicenseEditText = (TextView) findViewById(R.id.licenseEditText);
         mLicenseEditText.setText("I hereby agree to use my name, and only my name when using this app. I also agree to not abuse my privileges as an admin, semi-admin, or standard user, and will be subject to banning from the app if done so. This application is solely for the Fremont High School Robotics team, and any unknown users will immediately be banned from usage of the app. Please report any bugs or vulnerabilities of the app to the creators of the app. Use app responsibly.\n");
-
+        mSpinner = (Spinner) findViewById(R.id.subteamSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.subteam_names, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(adapter);
     }
 }
